6119a677232e3319ee35463c4c1e5ee44d69067b
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 00:57:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,7 +31,7 @@ def initial_round
   first_card = deal_card
   second_card = deal_card
   card_total = first_card + second_card
-  puts display_card_total(card_total)
+  display_card_total(card_total)
   return card_total
 end
 
